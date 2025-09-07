f030b93c836c962478a403cfdabee68c35ecc7e2
Yash <yash1812ys@gmail.com>
2025-09-07 15:31:46 +0530

    Implementing DLL

--- Cpp/Implementing Data Structures/DLL.cpp
+++ Cpp/Implementing Data Structures/DLL.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node{
+class node{
     public:
     int data;
     node* next;
@@ -12,281 +12,163 @@ struct node{
         data=data1;
         next=next1;
         prev=prev1;
-    } 
+    }
+
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+        prev=nullptr;
+    }
 };
 
+node* arraytoll(vector<int>& arr){
+    if(arr.size()==0) return nullptr;
 
-void printLL(node* head){
-    node* temp=head;
+    node* head= new node(arr[0]);
+    node* mover=head;
 
-    while(temp!=nullptr){
-        cout<< temp->data << "-->";
-        temp=temp->next;
+    for(int i=1;i<arr.size();i++){
+        mover->next= new node(arr[i],nullptr,mover);
+        mover=mover->next;
     }
 
-    cout<< "NULL";
+    return head;
 }
 
-void insertstart(node*& head){
-    int val;
-    cout<< "Enter value you want to insert:\n";
-    cin>> val;
-    node* newnode= new node(val,head,nullptr);
+node* insertatbegin(node* head,int value){
+    if(head==nullptr){
+        return new node(value);
+    }
 
-    if(head!=nullptr) head->prev=newnode;
-    head=newnode;
+
+    head->prev=new node(value,head,nullptr);
+    head=head->prev;
+
+    return head;
 }
 
-void dltstart(node*& head){
-    if(head==nullptr) return;
+node* dltstart(node*& head){
+    if(head==nullptr) return nullptr;
     else if(head->next==nullptr){
         delete head;
-        head=nullptr;
-        return;
+        return nullptr;
     }
 
-    node* temp=head;
 
-    head=temp->next;
+    head=head->next;
+    delete head->prev;
     head->prev=nullptr;
-    delete temp;
+
+    return head;
 }
 
-void insertend(node*& head){
-    node* temp=head;
-    int val;
-    cout<< "Enter value you want to insert:\n";
-    cin>> val;
-
-    
-    node* newnode= new node(val,nullptr,nullptr);
-
+node* insertend(node* head,int value){
     if(head==nullptr){
-        head=newnode;
-        return;
+        head=new node(value);
+        return head;
     }
 
-    while(temp->next!=nullptr){
-        temp=temp->next;
-    }
+    node* temp=head;
 
-    temp->next=newnode;
-    newnode->prev=temp;
+    while(temp->next!=nullptr) temp=temp->next;
+    temp->next=new node(value,nullptr,temp);
+    return head;
 }
 
-void dltend(node*& head){
-    if(head==nullptr) return;
-    if(head->next==nullptr){
-        dltstart(head);
-        return;
+node* dltend(node* head){
+    if(head==nullptr) return head;
+    else if(head->next==nullptr){
+        delete head;
+        return nullptr;
     }
+
 
     node* temp=head;
 
     while(temp->next!=nullptr){
         temp=temp->next;
     }
-    temp->prev->next=nullptr; 
+
+    temp->prev->next=nullptr;
     delete temp;
+
+
+    return head;
 }
 
-void insertpos(node*& head){
-    int p;
-    cout<< "Enter the position:\n";
-    cin>> p;
-
-    if(p==1){
-        insertstart(head);
-    }
-    else{
-        int val;
-        cout<< "Enter value you want to insert\n";
-        cin>> val;
-        int cnt=1;
-        node* newnode=new node(val,nullptr,nullptr);
-        node* temp=head;
-        while(temp!=nullptr){
-
-              if(cnt==p){
-                newnode->prev=temp->prev;
-                newnode->next=temp;
-                temp->prev->next=newnode;
-                temp->prev=newnode;
-                return;
-            }
-            temp=temp->next;
-            cnt++;
-        }
-        // inserting at the end need to implemented
-    }
-}
-
-
-
-void dltpos(node*& head){
-    if(head==nullptr) return;
-
-    int pos;
-    cout<< "Enter position you want to dlt:\n";
-    cin>> pos;
-
-    if(pos==1){
-        dltstart(head);
-        return;
-    }
-    int cnt=1;
-
+void printdll(node* head){
     node* temp=head;
-
-
 
     while(temp!=nullptr){
-        if(cnt==pos){
-            temp->prev->next=temp->next;
-            if(temp->next!=nullptr)
-                temp->next->prev=temp->prev;
-            delete temp;
-            return;
-        }
-        temp=temp->next;
-        cnt++;
-    }
-}
-
-void insertbeforeval(node*& head){
-    node* temp=head;
-
-
-    int val;
-    cout<< "Enter the value\n";
-    cin>> val;
-
-    if(val == head->data){
-        int newval;
-        cout << "Enter the value you want to insert:\n";
-        cin >> newval;
-
-        node* newnode = new node(newval, head,nullptr);
-        head->prev=newnode;
-        head = newnode;
-        return;
-    }
-
-    int newval;
-    cout<< "Enter the value you want to insert:\n";
-    cin>> newval;
-
-
-    while(temp!=nullptr && temp->next != nullptr && temp->next->data!=val){
+        cout<< temp->data << " <-->" ;
         temp=temp->next;
     }
-
-    node* newnode = new node(newval,nullptr,nullptr);
-    if(temp!=nullptr){
-        newnode->next=temp->next;
-        newnode->prev=temp;
-        temp->next=newnode;
-        temp->next->next->prev=newnode;
-    }
-}
-
-
-void dltval(node*& head){
-     if(head==nullptr) return;
-     
-    
-     int val;
-     cout<< "Enter the value:\n";
-     cin>> val;
-     
-    if(head->data==val){
-       node* temp=head;
-       head=temp->next;
-       head->prev=nullptr;
-       delete temp;
-       return;
-    }
-     node* temp=head;
-
-     while(temp!=nullptr){
-
-        if(temp->data==val){
-
-            if(temp->next==nullptr) dltend(head);
-            else{
-                temp->prev->next=temp->next;
-                temp->next->prev=temp->prev;
-                delete temp;
-                return;
-            }
-        }
-        temp=temp->next;
-}
+    cout<< "NULL" << endl;
 }
 
 
 int main(){
-    node* head=nullptr;
-    node* mover=nullptr;
-    int choice;
-    int n;
-
-    cout<< "Enter number of elements you want to initialize LL with:"<< endl;
-    cin>> n;
-
-    if(n>0){
-    cout<< "Enter the vals:\n";
-        int val;
-        cin>> val;
-        node* newnode= new node(val,nullptr,nullptr);
-        head=newnode;
-        mover=head;
-
-    for(int i=1;i<n;i++){
-        int val;
-        cin>> val;
-        node* newnode1= new node(val,nullptr,mover);
-        mover->next=newnode1;
-        mover=mover->next;
-    }
-}
-
-
-
-    while(true){
-        cout<< "\n1.Insert a Value at the start\n2.Delete Start\n3.Insert at end\n4.Delete end\n5.Insert at position\n6.Delete at position\n7.Insert element before value x\n8.Delete node with value x\n9.Print the linked list\nAnything else to stop:\n";
+    // creating a ll
+    // array to ll
+    vector<int> arr;
+    int choice=1;
+    
+    while(1){
+        cout<< "type 1 to create a newnode something else to stop:\n";
         cin>> choice;
+        if(choice!=1) break;
+
+        int temp;
+        cout<< "Enter the node value:\n";
+        cin>> temp;
+
+        arr.push_back(temp);
+    }
+
+    node* head=arraytoll(arr);
+
+
+    while(1){
+        int choice;
+        cout<<"0 - Insert a newnode at beginning\n1 - Delete start\n2 - Insert at end\n3 - Delete end\n4 - Insert at position\n9 - Print the dll\n";
+        cin>> choice;
+        
         switch(choice){
-        case 1:
-            insertstart(head);
-            break;
-        case 2:
-            dltstart(head);
-            break;
-        case 3:
-            insertend(head);
-            break;
-        case 4:
-            dltend(head);
-            break;
-        case 5:
-            insertpos(head);
-            break;
-        case 6:
-            dltpos(head);
-            break;
-        case 7:
-            insertbeforeval(head);
-            break;
-        case 8:
-            dltval(head);
-            break;
-        case 9:
-            printLL(head);
-            break;
-        default:
-            return 0;
+            case 0:
+                int val;
+                cout<< "Enter the value you want to insert:\n";
+                cin>> val;
+                head=insertatbegin(head,val);
+                break;
+            case 1:
+                dltstart(head);
+                break;
+            case 2:
+                int val;
+                cout<< "Enter the value you want to insert:\n";
+                cin>> val;
+                insertend(head,val);
+                break;
+            case 3:
+                dltend(head);
+                break;
+            case 4:
+                int pos;
+                cout<< "Enter the position you want to insert new node at:\n";
+                cin>> pos;
+                int val;
+                cout<< "Enter the value you want to insert:\n";
+                cin>> val;
+
+                // insertpos(head,val,pos);
+                break;
+                
+            case 9:
+                printdll(head);
+                break;
+            default:
+                return 0;
         }
     }
-    return 0;
 }
